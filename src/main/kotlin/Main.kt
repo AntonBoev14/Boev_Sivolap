2c1c811ef75d8ce96db46a9cc19d660c1c783423
KATO140_ <antonboev421@gmail.com>
2025-09-06 13:09:57 +0300

    pr.1 Boev and Sivolap

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,31 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Введите строку: ")
+    val text = readln()
 
+
+    if (text())
+    {
+        println("Вы ввели пустую строку")
+    }
+    var result = ""
+    var currentChar = text[0]
+    var count = 1
+
+    for(i in 1 until text)
+    {
+        if(text[i] == currentChar)
+        {
+            count++
+        }
+        else {
+            result += if (count > 1) "$currentChar$count " else "$currentChar"
+            currentChar = text[i]
+            count = 1
+        }
+    }
+    result += if(count > 1)"$currentChar$count" else"$currentChar"
+
+    println("Результат: $result ")
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
